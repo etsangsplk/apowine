1d7755ae3e9d68137892151c8c108465be8eeef4
Dimitri Stiliadis <dstiliadis@gmail.com>
2018-05-23 07:55:02 -0700

    Additional cleanups and policy updates

--- source/version/version.go
+++ source/version/version.go
@@ -4,4 +4,4 @@ package version
 const VERSION = "0.11"
 
 // REVISION is the revision of apowine
-const REVISION = "ca5588b3030c67d9e7cbfdabcaa07e051a4ded2a"
+const REVISION = "83ca8bc9f28f444c7a8ca04b4bbdf776a54b29d3"
